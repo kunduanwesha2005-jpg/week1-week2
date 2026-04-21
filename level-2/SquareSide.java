02c8102bb9a99cd454c12955b72af1c4571c92a3
kunduanwesha2005 <kunduanwesha20052gmail.com>
2026-04-21 19:39:43 +0530

    Anwesha added level-2 problem-3

---
+++ level-2/SquareSide.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SquareSide {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the perimeter: ");
+        double perimeter = scanner.nextDouble();
+
+        double side = perimeter / 4;
+
+        System.out.println(
+            "The length of the side is " + side +
+            " whose perimeter is " + perimeter
+        );
+
+        scanner.close();
+    }
+}
